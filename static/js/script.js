182c4010bd36e455b05178c6991c6fcace531faf
David Martin <davidjmartin01@gmail.com>
2017-05-21 20:55:40 -0700

    added semicolons

--- static/js/script.js
+++ static/js/script.js
@@ -4,15 +4,15 @@ var app = {
     baseURL: 'https://www.youtube.com/embed/?list={0}&index={1}',
     queryStrings: '&amp;t=15&amp;wmode=transparent&amp;autoplay=1&amp;rel=0&amp;showinfo=0&amp;iv_load_policy=3&amp;showsearch=0&amp;autohide=1&amp;controls=0&amp;wadsworth=1',
     iframeSrc: '',
-}
+};
 
 var generateRandom = function(num) {
     return Math.floor(Math.random() * num);
 };
 
 var setURL = function(){
-        return app.baseURL.format(app.playlist.id, app.index) + app.queryStrings
-    }
+        return app.baseURL.format(app.playlist.id, app.index) + app.queryStrings;
+};
 
 String.prototype.format = function() {
     var string = this;
@@ -27,9 +27,9 @@ String.prototype.format = function() {
 app.playlist = (function(){
   var loc = generateRandom(playlists.length);
   return playlists[loc];
-}())
+}());
 
-app.index = generateRandom(app.playlist.max)
+app.index = generateRandom(app.playlist.max);
 
 app.iframeSrc = setURL();
 
@@ -37,15 +37,14 @@ $(function () {
   $('#player').attr('src', app.iframeSrc)
 
   $('#back').click(function(){
-      app.index--
+      app.index--;
       app.iframeSrc = setURL();
     $('#player').attr('src', app.iframeSrc);
   });
 
   $('#forward').click(function(){
-      app.index++
+      app.index++;
       app.iframeSrc = setURL();
     $('#player').attr('src', app.iframeSrc);
   });
-})
-
+});
